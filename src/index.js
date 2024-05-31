990189f91a40d2a3e814840b9134203d41117a88
Anupam <9582anupamk@gmail.com>
2024-06-01 01:55:33 +0530

    add dotenv, server configs, setup express on port 3000

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { PORT } = require('./config/serverConfig');
+
+const setupAndStartServer = async () => {
+    
+    // create the express object
+    
+    const app = express(); 
+    const PORT = 3000;
+    
+    app.listen(PORT, () => {
+        console.log(`Server Started at ${PORT}`);
+    });
+}
+ 
+
+setupAndStartServer();
+
