b615448a64691e9a66fd5002f99c43e40616521e
Anupam <9582anupamk@gmail.com>
2024-06-02 17:52:58 +0530

    completed city repository

--- src/repositories/city-repository.js
+++ src/repositories/city-repository.js
@@ -19,11 +19,36 @@ class CityRepository {
                     id: cityId
                 }
             });
+            return true;
         }
         catch (error) {
             console.log(error)
         }
-    } 
+    }
+    
+    async updateCity (cityId, data) {
+        try {
+            const city = await City.update(data, {
+                where: {
+                    id: cityId
+                }
+            });
+            return city;
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+
+    async getCity () {
+        try {
+            const city = await City.findAll();
+            return city;
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
 }
 
 module.exports = CityRepository;
