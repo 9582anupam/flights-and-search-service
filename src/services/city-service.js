81a3e8ad348f83e358e2579d77dc4b0f62678780
Anupam <9582anupamk@gmail.com>
2024-06-02 19:13:21 +0530

    add city service

---
+++ src/services/city-service.js
@@ -0,0 +1,46 @@
+const { CityRepository } = require('../repositories/index');
+
+class CityService {
+    constructor() {
+        this.cityRepository = new CityRepository();
+    }
+
+    async createCity(data) {
+        try {
+            const city = await this.cityRepository.create(data);
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+
+    async deleteCity(cityId) {
+        try {
+            const city = await this.cityRepository.delete(cityId);
+            return city;
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+
+    async updateCity(cityId, data) {
+        try {
+            const city = await this.cityRepository.update(cityId, data);
+            return city;
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+
+    async getCity(cityId) {
+        try {
+            const city = await this.cityRepository.getCity(cityId);
+            return city;
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+}
